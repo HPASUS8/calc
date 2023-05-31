4f57ea8cc0450cf53c0204a0e6a772165586297d
HPASUS8 <76400381+HPASUS8@users.noreply.github.com>
2023-05-31 12:38:05 +0300

    Обновление 1.0

---
+++ calc.py
@@ -0,0 +1,19 @@
+math = input('Привет! Выбери действие и нпаиши это действие: умножение, деление, сложение и вычитание: ')
+first = int(input('Хорошо... Теперь напиши первое число: '))
+second = int(input('Окей... Теперь напиши второе число: '))
+
+if math.lower() == 'умножение':
+    multi = first * second
+    print(f'Итак... Умножение {first} на {second} будет... {multi}!')
+
+if math.lower() == 'деление':
+    div = first / second
+    print(f'Итак... Деление {first} на {second} будет... {div}!')
+
+if math.lower() == 'сложение':
+    plus = first + second
+    print(f'Итак... Сумма {first} и {second} равна... {plus}!')
+
+if math.lower() == 'вычитание':
+    minus = first - second
+    print(f'Итак... Разность {first} и {second} равна... {minus}!')
